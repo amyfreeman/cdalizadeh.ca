ef364ad8c6ff0ab49f17762c28498aee18f21844
Cameron Alizadeh <cdalizadeh@gmail.com>
2017-08-28 15:07:41 -0400

    use express static, add temporary landing page

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-
-// [START hello_world]
-// Say hello!
-app.get('/', (req, res) => {
-  res.status(200).send('<h1>cdalizadeh in tha house</h1>');
-});
-// [END hello_world]
+app.use(express.static('public'))
 
 if (module === require.main) {
-  // [START server]
-  // Start the server
   const server = app.listen(process.env.PORT || 8081, () => {
     const port = server.address().port;
     console.log(`App listening on port ${port}`);
   });
-  // [END server]
 }
